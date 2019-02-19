6f9100cff3b27ebb78ba7b848978ed29f24e13ac
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-02-19 17:03:55 -0300

    Formulario das questoes

--- Compleaks/compleaks/questoes/forms.py
+++ Compleaks/compleaks/questoes/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
+from wtforms import (StringField, SubmitField, BooleanField, SelectField)
+from wtforms.validators import DataRequired
 
-class AdicionarQuestaoForm(FlaskForm):
+class AdicionarQuestaoForm(FlaskForm):	
+	enunciado = StringField("Enunciado:", validators=[DataRequired()])
+	disciplina = SelectField("Disciplina:", validators=[DataRequired()])
+	submit = SubmitField("Adicionar")
 
-	pass
+class AdicionarAlternativaForm(FlaskForm):
+	conteudo = StringField("Conteudo:", validators=[DataRequired()])
+	correta = BooleanField("Correta:")
+	submit = SubmitField("Adicionar")
 
 class EditarQuestaoForm(FlaskForm):
+	novo_enunciado = StringField("Enunciado:", validators=[DataRequired()])
+	nova_disciplina = SelectField("Disciplina:", validators=[DataRequired()])
+	submit = SubmitField("Editar")
 
-	pass
+class EditarAlternativaForm(FlaskForm):
+	novo_conteudo = StringField("Conteudo:", validators=[DataRequired()])
+	novo_correta = BooleanField("Correta:")
+	submit = SubmitField("Editar")
