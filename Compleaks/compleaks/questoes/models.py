d644b585ba105686a4716a6869137d3b6b4df576
guilhermebezerrads <40393853+guilhermebezerrads@users.noreply.github.com>
2019-02-19 15:12:47 -0300

    Models de questão e alternativa para futuros simulados

---
+++ Compleaks/compleaks/questoes/models.py
@@ -0,0 +1,36 @@
+from compleaks import db
+from compleaks.disciplinas.models import Disciplina
+from compleaks.usuarios.models import Usuario
+
+class Questao(db.Model):
+
+	__tablename__ = 'questoes'
+
+	id = db.Column(db.Integer, primary_key=True)
+	enunciado = db.Column(db.Text, nullable=False)
+	data_criacao = db.Column(db.DateTime, nullable=False)
+
+	disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplinas.id'), nullable=False)
+	usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+
+	alternativas = db.relationship('alternativas', backref='questao', uselist=True)
+
+	def __init__(self, enunciado, disciplina_id):
+		self.enunciado = enunciado
+		self.disciplina_id = disciplina_id
+
+class Alternativa(object):
+	
+	__tablename__ = 'alternativas'
+
+	id = db.Column(db.Integer, primary_key=True)
+	conteudo = db.Column(db.Text, nullable=False)
+	correta = db.Column(db.Boolean, default=False)
+
+	questao_id = db.Column(db.Integer, db.ForeignKey('questoes.id'), nullable=False)
+
+	def __init__(self, conteudo, questao_id, correta):
+		self.conteudo = conteudo
+		self.questao_id = questao_id
+		self.correta = correta
+
