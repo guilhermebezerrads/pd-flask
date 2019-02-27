ce2ddc10745a7197750a69e5dc07ba0c186009f5
Gabriel Machado <gabriel.galoma@gmail.com>
2019-02-27 18:05:20 -0300

    Inicio da criação de envio de feedback

---
+++ Compleaks/compleaks/principal/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, SubmitField, SelectField, TextAreaField)
+from wtforms.validators import DataRequired, Length, Email
+
+class FeedBackForm(FlaskForm):	
+
+	texto = TextAreaField("Email de contato:", validators=[DataRequired(), 
+				Length( max=1000, message="Máximo de 400 por favor!")])
+	tipo = SelectField("Tipo de erro:", validators=[DataRequired()],
+				 choices=[("Sugestão de melhoria","Sugestão de melhoria"),
+				  ("Reclamações","Reclamações"), ("Repostar Erro","Repostar Erro"),
+				  ("Outro","Outro")])
+	titulo = StringField("Titulo", validators=[DataRequired(), 
+			Length(max=60, message="Minimo de 4 caracteres e máximo de 120 por favor!")])
+	email = StringField("Seu email para contato", validators=[DataRequired(), Email()])
+	
+	submit = SubmitField("Enviar FeedBack")
--- Compleaks/compleaks/principal/views.py
+++ Compleaks/compleaks/principal/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, Blueprint, url_for
+from flask import render_template, Blueprint, url_for, flash
 from compleaks import db
 from compleaks.usuarios.forms import LoginForm
+from compleaks.principal.forms import FeedBackForm
 
 principal = Blueprint('principal', __name__)
 
@@ -9,6 +10,31 @@ def index():
 	form_login = LoginForm()
 	return render_template('index.html', form_login=form_login)
 
+def feedback_mensage(form):
+	
+	msg = Message(str(form.titulo.data),
+                  sender=str(form.email.data),
+                  recipients=["jinformatica471@gmail.com"])
+
+	msg.body = f'''Um feedback do tipo: {form.tipo.data}
+	Enviado por: {form.email.data}
+	Contendo o seguinte conteúdo:
+	{form.texto.data}
+Se você não solicitou esta modificação, apenas ignore esse email e nenhuma mudança será feita.
+'''
+	mail.send(msg)
+
+
+@principal.route('/feedback')
+def index():
+	form_login = LoginForm()
+	form = FeedBackForm()
+
+	if form.validate_on_submit:
+		feedback_mensage(form)
+		flash("Email enviado com sucesso.", "success")
+
+	return render_template('feedback.html', form_login=form_login, form=form)
 
 # @app.errorhandler(404)
 # def page_not_found(e):
