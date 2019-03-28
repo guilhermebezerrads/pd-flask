f7e30eea4b6af1bbac7e93cea0e3138920446dc3
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-03-28 14:52:50 -0300

    js para modificar display de foms de edição

--- Compleaks/compleaks/static/js/style.js
+++ Compleaks/compleaks/static/js/style.js
@@ -1,17 +1,24 @@
 function formulario_editar_comentario(elemento1){
     var display1 = document.getElementById(elemento1).style.display;
-    var display2 = document.getElementById('ver_comentario').style.display;
     if(display1 == "none"){
         document.getElementById(elemento1).style.display = 'block';
     }else{
         document.getElementById(elemento1).style.display = 'none';
     }  
-    if(display2){
-        document.getElementById('ver_comentario').style.display = 'none';
-    }  
 }
 
 function responder_comentario(elemento1){
+    var display1 = document.getElementById(elemento1).style.display;
+
+    if(display1 == "none"){
+        document.getElementById(elemento1).style.display = 'block';
+    }else{
+        document.getElementById(elemento1).style.display = 'none';
+    }
+
+}
+
+function editar_resposta(elemento1){
     var display1 = document.getElementById(elemento1).style.display;
 
     if(display1 == "none"){
