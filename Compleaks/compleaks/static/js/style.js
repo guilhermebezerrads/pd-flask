6e56a55f995283976252708568d7013a0b8d7cda
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-03-29 11:34:07 -0300

    CKEDITOR funções JS

--- Compleaks/compleaks/static/js/style.js
+++ Compleaks/compleaks/static/js/style.js
@@ -9,7 +9,6 @@ function formulario_editar_comentario(elemento1){
 
 function responder_comentario(elemento1){
     var display1 = document.getElementById(elemento1).style.display;
-
     if(display1 == "none"){
         document.getElementById(elemento1).style.display = 'block';
     }else{
@@ -20,11 +19,18 @@ function responder_comentario(elemento1){
 
 function editar_resposta(elemento1){
     var display1 = document.getElementById(elemento1).style.display;
-
     if(display1 == "none"){
         document.getElementById(elemento1).style.display = 'block';
     }else{
         document.getElementById(elemento1).style.display = 'none';
     }
 
+}
+
+function adicionaCKEDITOR(elemento){
+    CKEDITOR.replace( elemento , { width: "50vw", height: 'auto'});
+}
+
+function respAdicionaCKEDITOR(elemento){
+    CKEDITOR.replace( elemento , { width: "40vw", height: 'auto'});
 }
